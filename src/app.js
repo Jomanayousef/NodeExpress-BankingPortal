704294a29c6a9d73e466d3c3c71413c68038328c
Jomana Yousef <jojo.girl2012@gmail.com>
2020-06-12 16:23:39 +0300

    Completed module

--- src/app.js
+++ src/app.js
@@ -10,10 +10,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname , 'public')));
 
-app.get('/', (req, res) => {
-    res.render('index.ejs',
-     { title: "index"})
-});
+app.get('/', (req, res) => {res.render('index', { title: "index"})});
 
 
 app.listen(3000, ()=> console.log('PS Project Running on port 3000!'));
